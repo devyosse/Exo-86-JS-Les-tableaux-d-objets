89acdce62884119ac3fe1f52f391d85c72a0b1a1
devyosse <narutoshippuden884@gmail.com>
2021-11-30 13:54:48 +0100

    exo 86

--- app.js
+++ app.js
@@ -31,8 +31,24 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
+
+
+
+for(y = 0; y < voitures.collection.length; y++) {
+    let para = document.getElementById('para');
+    let span = document.createElement('span');
+    span.style.color = voitures.collection[i].color;
+    span.innerHTML = voitures.collection[y].color + ", ";
+    voitures.collection[y].ouvrirPorte();
+    para.appendChild(span);
+
+}
+
+
+
+
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
